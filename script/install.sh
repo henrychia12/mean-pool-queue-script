3aca168d44911be4c16a291f4407abb0ecb39211
henrychia12 <henry_chia21@hotmail.co.uk>
2019-07-10 13:14:11 +0000

    angular service file and install script updated

--- script/install.sh
+++ script/install.sh
@@ -16,17 +16,24 @@ sudo usermod --shell /bin/bash meanadm
 # briefly carries a task on user - meanadm to git clone backend repo
 sudo su - meanadm -c "git clone -b Developer https://github.com/Nboaram/TeamAPoolProjectBackend.git && git clone -b Developer https://github.com/Nboaram/TeamAPoolProjectUI.git"
 
-# install  npm dependencies as meanadm
+# install npm dependencies as meanadm in front/back end repo
 sudo su - meanadm -c "cd TeamAPoolProjectBackend && npm install"
+sudo su - meanadm -c "cd TeamAPoolProjectUI && npm install"
 
-# delete mongodb service file if already exists
+# installing angular globally
+yes | sudo npm -g install @angular/cli
+
+# delete mongodb and angular service file if already exists
 sudo rm -f /etc/systemd/system/mongodb.service
+sudo rm -f /etc/systemd/system/angular.service
 
-# copy mongodb service file to systemd
+# copy mongodb and angular service file to systemd
 sudo cp ../mongodb.service /etc/systemd/system
+sudo cp ../angular.service /etc/systemd/system
 
-# mongodb service file added. systemd reload required and start mongodb
+# new service files added. systemd reload required to start angular and mongodb
 sudo systemctl daemon-reload
+sudo systemctl restart angular
 sudo systemctl restart mongodb
 
 # install angular
